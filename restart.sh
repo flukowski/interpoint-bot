d9c2aa26fd29d8562e61190cfc58588568ec3cf4
Fluke <>
2020-11-16 20:19:53 +0530

    Fix restart script

--- restart.sh
+++ restart.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-git pull origin master && (killall python3 || true) && source ~/.bash_profile && (nohup python -u ralf.py > out.log 2>&1 &)
+git pull origin master && (killall python3 || true) && source ~/.bash_profile && (nohup python3 -u ralf.py > out.log 2>&1 &)
